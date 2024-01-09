18e12dd462526e8dae968e2c052df566b4cc2692
TimurMeshchenko <matsuokisho@gmail.com>
2024-01-10 00:18:55 +0300

    a

--- django/remanga/remanga_site/remanga/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class RemangaConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'remanga'
--- django/remanga/remanga_site/remanga/static/catalog/js/filters/filters.js
+++
@@ -1,8 +0,0 @@
-class Filters {
-
-  main(data_array) {
-    new Drop_menu_filters(data_array);
-    new Input_range_filters();
-    new Chapters_filters();
-  }
-}
